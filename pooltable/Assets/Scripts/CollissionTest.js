b2a009849fda2489939ba35b1bd08756a0768aa4
RSlomp <slom1500@student.nhl.nl>
2016-09-28 13:45:58 +0200

    V2.0.1

--- pooltable/Assets/Scripts/CollissionTest.js
+++ pooltable/Assets/Scripts/CollissionTest.js
@@ -1,7 +1,6 @@
 ﻿#pragma strict
 
 var forceAmount : Vector3;
-var removeForce : Vector3;
 
 function Start () {
 
@@ -10,6 +9,12 @@ function Start () {
 function Update () {
 	if(Input.GetButtonDown("Jump"))
 		GetComponent.<Rigidbody>().AddForce(forceAmount);
-	if(Input.GetButtonDown("Fire1"))
-		GetComponent.<Rigidbody>().AddForce(removeForce);
+	
+}
+
+function OnCollisionEnter (col : Collision)
+{
+    if(col.gameObject.name == "TableSide1" || col.gameObject.name == "TableSide2" || col.gameObject.name == "TableSide3" || col.gameObject.name == "TableSide4") {
+		GetComponent.<Rigidbody>().AddForce((-(forceAmount))*2);
+    }
 }
