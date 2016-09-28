75de4d1b78dec3ec399f760c96ea90c60c087954
RSlomp <slom1500@student.nhl.nl>
2016-09-28 12:39:31 +0200

    V2

---
+++ pooltable/Assets/Scripts/CollissionTest.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var forceAmount : Vector3;
+var removeForce : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetButtonDown("Jump"))
+		GetComponent.<Rigidbody>().AddForce(forceAmount);
+	if(Input.GetButtonDown("Fire1"))
+		GetComponent.<Rigidbody>().AddForce(removeForce);
+}
