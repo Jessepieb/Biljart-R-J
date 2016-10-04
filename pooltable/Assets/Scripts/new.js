9e0f04d606fe1292e0dd1fd3b0143fbf9441a578
RSlomp <slom1500@student.nhl.nl>
2016-10-04 14:42:35 +0200

    rigidbody script test v2.2.0

---
+++ pooltable/Assets/Scripts/new.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var ForceAmount : Vector3;
+function Start () {
+
+}
+
+function Update () {
+ if (Input.GetButtonDown("Forward"))
+ GetComponent.<Rigidbody>().AddForce(ForceAmount * 10);
+
+}
+
