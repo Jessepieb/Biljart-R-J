54a6a61f63588a2d90a7014e2d6d654254b7b1f2
PiebMaster420 <jessepiebenga@gmail.com>
2016-10-14 10:54:53 +0200

    nietwerkende js

---
+++ pooltable/Assets/Scripts/collision.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var rb: Rigidbody;
+var sphere : SphereCollider;
+public var attachedRigidbody : Rigidbody;
+
+public var radius : float;
+radius = 0.8;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter ( TableSide4 : Collider) {
+		
+}
---
+++ pooltable/Assets/Scripts/collisiondebug.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function OnCollisionEnter(theCollision : Collision){
+if (theCollision.gameObject.name == "TableTop"){
+Debug.Log("Hit the Floor");
+}
+else if (theCollision.gameObject.name == "TableSidde4"){
+Debug.Log("hit tableside4");
+}
+}
