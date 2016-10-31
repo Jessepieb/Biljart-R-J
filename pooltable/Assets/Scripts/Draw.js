fc849a2c2fc97eef615b1cb227a8866e83b41c95
RSlomp <slom1500@student.nhl.nl>
2016-10-31 20:33:53 +0100

    Pool new?

---
+++ pooltable/Assets/Scripts/Draw.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+
+function Start () {
+
+}
+
+function Update() {
+    if(Input.GetMouseButtonDown(0)){
+       move();
+    }
+}
+
+function move(){
+	for(var i = 0; i < 60; i++){
+        	transform.position += Vector3(0,0,0.12);
+        }
+        GetComponent.<Renderer>().enabled = false;
+        yield WaitForSeconds(1);
+        transform.position = ((GameObject.Find("Ball1").transform.position) - Vector3(0, -1, 9));
+        transform.Rotate = Vector3(181.5, 0.0, 90.0, Space.Self);
+}
---
+++ pooltable/Assets/Scripts/Rotate.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Z)){
+		GetComponent.<Renderer>().enabled = true;
+	}
+	if(Input.GetKeyDown(KeyCode.X)){
+		GetComponent.<Renderer>().enabled = false;
+	}
+}
