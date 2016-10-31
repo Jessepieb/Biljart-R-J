82641677221dca5fd343a3851295d562d0bcfb56
PiebMaster420 <jessepiebenga@gmail.com>
2016-10-31 20:34:21 +0100

    sort of  working JS

--- pooltable/Assets/Scripts/collisiondebug.js
+++ pooltable/Assets/Scripts/collisiondebug.js
@@ -1,10 +1,22 @@
 ﻿#pragma strict
-
 function OnCollisionEnter(theCollision : Collision){
 if (theCollision.gameObject.name == "TableTop"){
 Debug.Log("Hit the Floor");
 }
-else if (theCollision.gameObject.name == "TableSidde4"){
-Debug.Log("hit tableside4");
+
+if (theCollision.gameObject.tag == "TableSide"){
+Debug.Log("hit tableside");
+}
+if (theCollision.gameObject.tag == "TableSide") {
+/*var angle;
+angle = (Vector3.Angle);
+Debug.Log(Vector3.Angle);*/
+transform.Rotate (Vector3.right,180);
+}
+if (theCollision.gameObject.tag == "sphere") {
+/*var angle;
+angle = (Vector3.Angle);
+Debug.Log(Vector3.Angle);*/
+transform.Rotate (Vector3.right,180);
 }
 }
