bf5874b1f47d622f3c7eab4248b2cfe000b7d39a
RSlomp <slom1500@student.nhl.nl>
2016-11-09 12:39:56 +0100

    Pool

--- pooltable/Build/Development/UnityLoader.js
+++ pooltable/Build/Development/UnityLoader.js
@@ -3499,8 +3499,8 @@ Module.expectedDataFileDownloads++;
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
-      var REMOTE_PACKAGE_SIZE = 17416763;
-      var PACKAGE_UUID = 'aa9d8011-c208-4fda-b8a9-dc62e307b323';
+      var REMOTE_PACKAGE_SIZE = 18339360;
+      var PACKAGE_UUID = '0769d609-8d65-45fe-ae65-74b2d57cea8e';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -3604,12 +3604,12 @@ Module['FS_createPath']('/Managed/mono', '2.0', true, true);
       },
     };
 
-      new DataRequest(0, 14553752, 0, 0).open('GET', '/data.unity3d');
-    new DataRequest(14553752, 14553773, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
-    new DataRequest(14553773, 14559822, 0, 0).open('GET', '/preserved_derived_types.xml');
-    new DataRequest(14559822, 16514166, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
-    new DataRequest(16514166, 17389138, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(17389138, 17416763, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
+      new DataRequest(0, 15474135, 0, 0).open('GET', '/data.unity3d');
+    new DataRequest(15474135, 15474156, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(15474156, 15480291, 0, 0).open('GET', '/preserved_derived_types.xml');
+    new DataRequest(15480291, 17436763, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
+    new DataRequest(17436763, 18311735, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(18311735, 18339360, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
 
     function processPackageData(arrayBuffer) {
       Module.finishedDataFileDownloads++;
